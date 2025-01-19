7ac1b06ecff236a24430920c483ab20c72f4c432
kirill <pusedzhuse228@gmail.com>
2025-01-19 14:31:28 +0300

    Поменял номер версии, теперь это гордая 2.0, и также появились зачатки под локализацию

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.platon.easymusicandroid"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
